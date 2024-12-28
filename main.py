55063781069dab383d3483718e70682df73726ce
maxwell7774 <maxwell7774@gmail.com>
2024-12-28 15:47:13 -0700

    Adds get_char_dict

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
+    num_characters = get_chars_dict(text)
     print(f"{num_words} words found in the document")
+    print(num_characters)
 
 
 def word_count(text):
     words = text.split()
     return len(words)
+
+
+def get_chars_dict(text):
+    characters = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
+    return characters
 
 
 def get_book_text(path):
