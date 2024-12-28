988808d0a201daa78e77fe3433a7f7095fcca5db
maxwell7774 <maxwell7774@gmail.com>
2024-12-28 15:35:21 -0700

    Adds word count function

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
+    print(f"{num_words} words found in the document")
+
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
