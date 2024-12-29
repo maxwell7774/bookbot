070304b735e9bf9b168da6fc02b10e10a6dc7b05
maxwell7774 <maxwell7774@gmail.com>
2024-12-28 21:55:46 -0700

    Adds the report to print out the stats in a uniform manner

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
-    num_characters = get_chars_dict(text)
-    print(f"{num_words} words found in the document")
-    print(num_characters)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+
+    print(f"{num_words} words found in the document\n")
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item["char"]}' character was found {item["num"]} times")
+
+    print("--- End report ---")
+
+
+def sort_on(dict):
+    return dict["num"]
 
 
 def word_count(text):
@@ -12,15 +26,23 @@ def word_count(text):
     return len(words)
 
 
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for c in num_chars_dict:
+        sorted_list.append({"char": c, "num": num_chars_dict[c]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
 def get_chars_dict(text):
-    characters = {}
-    lowered_text = text.lower()
-    for char in lowered_text:
-        if char in characters:
-            characters[char] += 1
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            characters[char] = 1
-    return characters
+            chars[lowered] = 1
+    return chars
 
 
 def get_book_text(path):
